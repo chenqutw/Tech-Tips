d969a7c704ad62ffd89f8f0e04ec278df3b60f14
JianingZheng <jnzheng@thoughtworks.com>
2012-09-05 09:00:33 +0800

    create a discussion while creating a tip, and some css modification

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -10,6 +10,7 @@ class TipsController < ApplicationController
   def create
     @tip = Tip.new(params[:tip])
     if @tip.save
+      create_discussion_with_tip(params[:tip][:title])
       redirect_to tips_path
     else
       render 'new'
@@ -18,5 +19,12 @@ class TipsController < ApplicationController
 
   def show
     @tip = Tip.find(params[:id])
+    @discussion = Discussion.find_by_title(@tip.title)
   end
+
+  private
+  def create_discussion_with_tip(title)
+     discussion = Discussion.create({title: title})
+  end
+
 end
